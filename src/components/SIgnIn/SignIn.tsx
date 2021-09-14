45932d7e6762ca22a6193a1cef268fa973e995a5
Bohdan <bohdan.kornatskyi@gmail.com>
2021-09-13 20:54:21 -0700

    added react-hook-form and implement error validation handling for both forms

--- src/components/SIgnIn/SignIn.tsx
+++ src/components/SIgnIn/SignIn.tsx
@@ -1,17 +1,62 @@
 import React, { ReactElement } from "react";
+import { useForm } from "react-hook-form";
 import "./SignIn.scss";
 
 interface Props {}
 
+type FormValues = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn({}: Props): ReactElement {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const displayFormError = (message?: string): ReactElement | undefined => {
+    if (message) {
+      return <p className="error">{message}</p>;
+    }
+  };
+
   return (
     <div className="signInContainer formContainer">
       <div className="form-header">
         <h2>Login to you account</h2>
       </div>
-      <form className="signInForm">
-        <input type="text" placeholder="Email" />
-        <input type="password" className="password" placeholder="Password" />
+      <form className="signInForm" onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}>
+        <input
+          {...register("email", {
+            required: { value: true, message: "Input your email!" },
+            maxLength: {
+              value: 30,
+              message: "Field length should be less then 30 chars!",
+            },
+          })}
+          type="text"
+          placeholder="Email"
+        />
+        {displayFormError(errors.email?.message)}
+
+        <input
+          {...register("password", {
+            required: { value: true, message: "Input your password!" },
+            maxLength: {
+              value: 30,
+              message: "Field length should be less then 30 chars!",
+            },
+          })}
+          type="password"
+          className="password"
+          placeholder="Password"
+        />
+        {displayFormError(errors.password?.message)}
+
         <input type="submit" className="button" value="Sign In" />
       </form>
 
